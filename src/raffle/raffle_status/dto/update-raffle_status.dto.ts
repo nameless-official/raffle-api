f519d054dd2da67c58a587f02f25e82525439578
Jonatan-ESG <jona.sandovalguerra@gmail.com>
2024-03-13 07:02:43 -0600

    :construction_worker:  Added error messages to UpdateRaffleStatusDto

--- src/raffle/raffle_status/dto/update-raffle_status.dto.ts
+++ src/raffle/raffle_status/dto/update-raffle_status.dto.ts
@@ -1,17 +1,21 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateRaffleStatusDto } from './create-raffle_status.dto';
-import { IsBoolean, IsString, IsInt } from 'class-validator';
+import { IsBoolean, IsString, IsInt, IsOptional, MaxLength } from 'class-validator';
 
 export class UpdateRaffleStatusDto extends PartialType(CreateRaffleStatusDto) {
-  @IsString()
+  @IsOptional()
+  @IsString({ message: 'Property code must be a string value' })
+  @MaxLength(50, { message: 'Property code must be a smaller than 50 characters ' })
   code?: string;
 
-  @IsString()
+  @IsOptional()
+  @IsString({ message: 'Property name must be a string value' })
+  @MaxLength(50, { message: 'Property name must be a smaller than 50 characters' })
   name?: string;
 
-  @IsInt()
+  @IsInt({ message: 'Property sort must be a integer value' })
   sort?: number;
 
-  @IsBoolean()
+  @IsBoolean({ message: 'Property is_finished must be a boolean value' })
   is_finished?: boolean;
 }
