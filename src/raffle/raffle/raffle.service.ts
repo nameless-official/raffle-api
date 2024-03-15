573a7f46111ff22f8837dc7771c3e98fbe28b00a
Jonatan-ESG <jona.sandovalguerra@gmail.com>
2024-03-15 00:22:37 -0600

    :construction_worker: Create Raffle Service updated

--- src/raffle/raffle/raffle.service.ts
+++ src/raffle/raffle/raffle.service.ts
@@ -1,9 +1,12 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { BadRequestException, HttpStatus, Inject, Injectable } from '@nestjs/common';
 import { CreateRaffleDto } from './dto/create-raffle.dto';
 import { UpdateRaffleDto } from './dto/update-raffle.dto';
 import { Raffle } from './entities/raffle.entity';
 import { Repository } from 'typeorm';
 import { BaseService } from 'src/common/service';
+import { CustomException } from 'src/common/exeptions/custom.exeption';
+import { RaffleStatusService } from '../raffle_status/raffle_status.service';
+import slugify from 'slugify';
 
 @Injectable()
 export class RaffleService extends BaseService<Raffle, CreateRaffleDto, UpdateRaffleDto> {
@@ -14,7 +17,38 @@ export class RaffleService extends BaseService<Raffle, CreateRaffleDto, UpdateRa
   constructor(
     @Inject('RAFFLE_REPOSITORY')
     private raffleRepository: Repository<Raffle>,
+    private raffleStatusService: RaffleStatusService,
   ) {
     super(raffleRepository);
+  }
+
+  async create(createRaffleDto: CreateRaffleDto) {
+    try {
+      if (Array.isArray(createRaffleDto)) {
+        throw new BadRequestException('Solo se debe crear un registro a la vez');
+      }
+
+      const validRecord = await this.validateEntity(createRaffleDto, this.createDTO);
+
+      if (validRecord instanceof BadRequestException) {
+        throw validRecord;
+      }
+      const { raffle_status_id: raffleStatusId } = createRaffleDto;
+
+      const newRaffle = this.raffleRepository.create(createRaffleDto);
+
+      const raffleStatus = await this.raffleStatusService.findOne(raffleStatusId);
+      if (!raffleStatus)
+        throw new CustomException(`The selected raffle ${raffleStatusId}, does not exists`, HttpStatus.NOT_FOUND);
+
+      newRaffle.raffleStatus = raffleStatus;
+
+      newRaffle.slug = slugify(newRaffle.name, { replacement: '-', lower: true, trim: true });
+
+      await this.raffleRepository.save(newRaffle);
+      return newRaffle;
+    } catch (error) {
+      this.serviceErrorHandler(error);
+    }
   }
 }
--- src/raffle/raffle_status/raffle_status.module.ts
+++ src/raffle/raffle_status/raffle_status.module.ts
@@ -9,5 +9,6 @@ import { AuthModule } from 'src/auth/auth.module';
   imports: [DatabaseModule, AuthModule],
   controllers: [RaffleStatusController],
   providers: [...raffleStatusProviders, RaffleStatusService],
+  exports: [RaffleStatusService],
 })
 export class RaffleStatusModule {}
