7de8517e869c6166592d43a8ebf090d0e3c6ffe5
Jonatan-ESG <jona.sandovalguerra@gmail.com>
2024-03-15 23:00:11 -0600

    :construction_worker: Added findOneBySlug endpoint

--- src/raffle/prize/prize.module.ts
+++ src/raffle/prize/prize.module.ts
@@ -9,5 +9,6 @@ import { AuthModule } from 'src/auth/auth.module';
   imports: [DatabaseModule, AuthModule],
   controllers: [PrizeController],
   providers: [...prizeProviders, PrizeService],
+  exports: [PrizeService],
 })
 export class PrizeModule {}
--- src/raffle/raffle/raffle.controller.ts
+++ src/raffle/raffle/raffle.controller.ts
@@ -5,14 +5,16 @@ import { UpdateRaffleDto } from './dto/update-raffle.dto';
 import { BaseController } from 'src/common/controller';
 import { Raffle } from './entities/raffle.entity';
 
-import { ApiBearerAuth, ApiHeader, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiHeader, ApiOperation, ApiParam, ApiTags } from '@nestjs/swagger';
 import { AuthGuard } from 'src/auth/guards/auth.guard';
+import { SearchDto } from 'src/common/dto/search.dto';
+import { PrizeService } from '../prize/prize.service';
 
 @ApiBearerAuth()
 @ApiTags('Raffle')
 @Controller('raffle')
 export class RaffleController extends BaseController<Raffle, CreateRaffleDto, UpdateRaffleDto> {
-  constructor(private readonly raffleService: RaffleService) {
+  constructor(private readonly raffleService: RaffleService, private readonly prizeService: PrizeService) {
     super(raffleService);
   }
 
@@ -29,6 +31,24 @@ export class RaffleController extends BaseController<Raffle, CreateRaffleDto, Up
   getPublishedRaffles(@Headers() headers: Record<string, string>) {
     try {
       return this.raffleService.getPublishedRaffles(this.getPagination(headers));
+    } catch (error) {
+      this.controllerErrorHandler(error);
+    }
+  }
+
+  @ApiParam({ name: 'slug', type: 'string' })
+  @ApiOperation({ summary: 'Gets an especific record by RecordId' })
+  @UseGuards(AuthGuard)
+  @Get(':slug')
+  async findOneBySlug(@Param('slug') slug: string) {
+    try {
+      const searchBySlugConditions: SearchDto = { conditions: [{ field: 'slug', operator: '=', value: slug }] };
+      const [raffle] = await this.raffleService.search(searchBySlugConditions, { limit: 1 });
+      const searchRafflePrizesConditions: SearchDto = {
+        conditions: [{ field: 'raffle_id', operator: '=', value: raffle.raffle_id }],
+      };
+      const prizes = await this.prizeService.search(searchRafflePrizesConditions, {});
+      return { ...raffle, prizes };
     } catch (error) {
       this.controllerErrorHandler(error);
     }
