cc79dbd556ab23c2aa80b71f6672095ea0504988
Jonatan-ESG <jona.sandovalguerra@gmail.com>
2024-03-15 23:35:02 -0600

    :construction_worker: Added Roles limitation to Prize and PrizeLevel Controllers

--- src/raffle/prize_level/prize_level.controller.ts
+++ src/raffle/prize_level/prize_level.controller.ts
@@ -6,8 +6,10 @@ import { BaseController } from 'src/common/controller';
 import { PrizeLevel } from './entities/prize_level.entity';
 
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { Roles } from 'src/role/role.decorator';
 
 @ApiBearerAuth()
+@Roles('admin')
 @ApiTags('PrizeLevel')
 @Controller('prize_level')
 export class PrizeLevelController extends BaseController<PrizeLevel, CreatePrizeLevelDto, UpdatePrizeLevelDto> {
